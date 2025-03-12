a8f1ef342429c7dbed79a646969ec165b8dd71f6
Asher Mills <asherpmills@gmail.com>
2025-03-12 17:05:58 -0400

    Fixed MultipleChoice component

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
 export function MultipleChoiceQuestion({
     options,
     expectedAnswer,
@@ -7,9 +7,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Options:</Form.Label>
+                <Form.Select value={answer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <span>{answer === expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
